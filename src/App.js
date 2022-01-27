44d2d04192e20463ab0107ac8bdac65076c25fe9
Summer <72228259+SummerinTech@users.noreply.github.com>
2022-01-27 10:49:23 -0800

    create basic styling
    added a background image and google fonts to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-	return <div className="App">Hello To Do List</div>;
+	return (
+		<div className="App">
+			<h1>To Do List</h1>
+			<div className="container"></div>
+		</div>
+	);
 }
 
 export default App;
