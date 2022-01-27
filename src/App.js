af3a60b8ef03e383e2fe0f3a409772c5142cb39a
Summer <72228259+SummerinTech@users.noreply.github.com>
2022-01-27 10:53:10 -0800

    set up first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Notepad from "./Components/Notepad";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>To Do List</h1>
-			<div className="container"></div>
+			<Notepad />
 		</div>
 	);
 }
---
+++ src/Components/Notepad.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Notepad = () => {
+	return <div className="Notepad"></div>;
+};
+
+export default Notepad;
