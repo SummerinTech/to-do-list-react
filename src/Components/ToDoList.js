07b06e3acafe88e76bfce0e3839ff707b743c54f
Summer <72228259+SummerinTech@users.noreply.github.com>
2022-01-27 12:30:06 -0800

    add strike-through functionality
    add the functionality to strike through the item when task is completed. Click again to unstrike item.

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ToDoList = ({ index, id, toDoItem }) => {
+	const [isFinished, setIsFinished] = useState(false);
+
+	const addLineThrough = () => {
+		setIsFinished((prevValue) => {
+			return !prevValue;
+		});
+	};
+
 	return (
-		<div key={index} className="ToDoList">
-			<li>{toDoItem}</li>
+		<div key={index} className="ToDoList" onClick={addLineThrough}>
+			<li style={{ textDecoration: isFinished ? "line-through" : "none" }}>
+				{toDoItem}
+			</li>
 		</div>
 	);
 };
